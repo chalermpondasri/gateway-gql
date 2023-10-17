3e7837a6b973d4182f4c396597fa84987e22651c
Chalermpon Dasri <chalermpon@ucconnect.co.th>
2023-10-17 16:08:02 +0700

    add localhost

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule)
     app.enableCors({
-        origin:  /doofin\.(rest|live|tv)$/,
+        origin:  [/doofin\.(rest|live|tv)$/, 'http://localhost'],
         credentials: true,
     })
     app.use(cookieParser())
