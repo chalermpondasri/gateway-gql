4143b0ad51c35a25549007de487773bcc54d7347
Chalermpon Dasri <chalermpon@ucconnect.co.th>
2023-10-17 18:21:25 +0700

    debug cors

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule)
     app.enableCors({
-        origin:  requestOrigin => {
+        origin: (requestOrigin, callback) => {
             Logger.log(requestOrigin, 'CORS')
-            return true
+            callback(null)
         },
         credentials: true,
     })
