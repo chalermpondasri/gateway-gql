389f48103406b3ab4317e124edb0c9089596e27f
chunhakrai <chunhakrai.b@gmail.com>
2023-11-28 15:32:16 +0700

    add log when error

--- src/services/cache/cache.service.ts
+++ src/services/cache/cache.service.ts
@@ -4,15 +4,23 @@ import {
     ICacheService,
 } from '@/services/cache/interface/service.interface'
 import {
+    catchError,
     from,
     map,
     Observable,
+    of,
 } from 'rxjs'
+import {
+    Logger,
+    LoggerService,
+} from '@nestjs/common'
 
 export class CacheService implements ICacheService{
+    private readonly _logger: LoggerService
     public constructor(
         private readonly _cacheMng: Cache
     ) {
+        this._logger = new Logger(CacheService.name)
     }
 
     public setCache(name: CacheName, value: string, ttl: null|number = null): void {
@@ -29,6 +37,10 @@ export class CacheService implements ICacheService{
         return from(this._cacheMng.store.get(name)).pipe(
             map(result => {
                 return result as string
+            }),
+            catchError(err => {
+                this._logger.error(`Get Cache : ${name} : ${err.toString()}`)
+                return of(null)
             })
         )
     }
