9e50e4276897a8e62d0851b97467255c160285ce
Chalermpon Dasri <chalermpon@ucconnect.co.th>
2024-05-02 05:01:04 +0000

    tune up

--- src/services/doofin-cms/cms.service.ts
+++ src/services/doofin-cms/cms.service.ts
@@ -62,6 +62,7 @@ import {
 import { capitalize } from 'lodash/fp'
 import {
     get,
+    isNil,
     reduce,
     size,
     some,
@@ -82,6 +83,7 @@ import { ICacheService } from '@/services/cache/interface/service.interface'
 import { IPlaybackRepository } from '@/repositories/playback/repository.interface'
 import { RentalStatus } from '@/types/enums/rental-status.enum'
 import { SearchService } from '@/services/search/services/search.service'
+import { LatestPlayedContentResponse } from '@/repositories/playback/latest-played-content.response'
 
 @Injectable()
 export class CmsService {
@@ -653,9 +655,20 @@ export class CmsService {
     }
 
     public getLatestPlayed(mediaContentId: number): Observable<LatestPlayedType> {
-
-        return this._playbackRepository.getLatestPlayedContent(this._requestContext.profileId).pipe(
-            map(v => v.find( element => mediaContentId === element.contentId)),
+        const cacheKey = `${this._playbackRepository.getLatestPlayedContent.name}_P:${this._requestContext.profileId}`
+        return this._cacheService.getCache(cacheKey).pipe(
+            mergeMap( dataString => {
+                if(!isNil(dataString)) {
+                    const json: unknown[] = JSON.parse(dataString)
+                    const result = plainToInstance(LatestPlayedContentResponse,json)
+                   return  of(result )
+                }
+                return this._playbackRepository.getLatestPlayedContent(this._requestContext.profileId).pipe(
+                    tap(result => this._cacheService.setCache(cacheKey, JSON.stringify(result), 15))
+                )
+            }),
+        ).pipe(
+            map((v) => v.find( element => mediaContentId === element.contentId)),
             defaultIfEmpty(null),
             map(v => {
                 if(!v) {
