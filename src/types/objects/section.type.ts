588e315bcedf53886180781e0d105839dd0c502a
chunhakrai <chunhakrai.b@gmail.com>
2024-05-16 01:33:07 +0700

    add latestPlayedFullDuration

--- src/types/objects/section.type.ts
+++ src/types/objects/section.type.ts
@@ -63,6 +63,8 @@ export class LatestPlayedType {
     public latestPlayedEpisodeId: number
     @Field()
     public latestPlayedPosition: number
+    @Field()
+    public latestPlayedFullDuration: number
 }
 
 @ObjectType()
