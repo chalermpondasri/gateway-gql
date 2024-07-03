6443604054009eacef1b927e1c286a048e7c45ec
chunhakrai <chunhakrai@ucconnect.co.th>
2024-07-03 13:30:57 +0700

    remove cache myList

--- src/repositories/auth/auth.repository.ts
+++ src/repositories/auth/auth.repository.ts
@@ -348,18 +348,9 @@ export class AuthRepository implements IAuthRepository {
     }
 
     public getMyList(profileId: string): Observable<MyListResponse[]> {
-        const cacheKey = `myList_${profileId}`
-        return this._cacheService.getCache(cacheKey).pipe(
-            mergeMap(cacheData => {
-                if(!isNil(cacheData)) {
-                    return of(JSON.parse(cacheData))
-                }
-                return from(this._axiosInstance.get(`/user/me/${profileId}/my-list`)).pipe(
-                    map(res => plainToInstance(MyListResponse, <Array<MyListResponse>>(res?.data?.subList ?? []))),
-                )
-            })
+        return from(this._axiosInstance.get(`/user/me/${profileId}/my-list`)).pipe(
+            map(res => plainToInstance(MyListResponse, <Array<MyListResponse>>(res?.data?.subList ?? []))),
         )
-
     }
 
     public addToMyList(profileId: string, programId: string): Observable<MyListResponse[]> {
